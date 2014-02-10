87eaeca5ec273fb909963bed4eb61112e1ba05d1
Bernd Mathiske <bernd@mesosphere.io>
2014-02-10 16:22:27 +0100

    Adding project files.

---
+++ project/Build.scala
@@ -0,0 +1,97 @@
+import sbt._
+import Keys._
+
+import com.typesafe.sbt.SbtScalariform._
+import scalariform.formatter.preferences._
+import sbtunidoc.Plugin._
+
+object MesosaurusBuild extends Build {
+
+//////////////////////////////////////////////////////////////////////////////
+// PROJECT INFO
+//////////////////////////////////////////////////////////////////////////////
+
+  val ORGANIZATION    = "io.mesosphere"
+  val PROJECT_NAME    = "mesosaurus"
+  val PROJECT_VERSION = "0.1.0"
+  val SCALA_VERSION   = "2.10.3"
+
+
+//////////////////////////////////////////////////////////////////////////////
+// NATIVE LIBRARY PATHS
+//////////////////////////////////////////////////////////////////////////////
+
+  val pathToMesosLibs = "/usr/local/lib"
+
+//////////////////////////////////////////////////////////////////////////////
+// ROOT PROJECT
+//////////////////////////////////////////////////////////////////////////////
+
+  lazy val root = Project(
+    id = PROJECT_NAME,
+    base = file("."),
+    settings = commonSettings
+  )
+
+
+//////////////////////////////////////////////////////////////////////////////
+// SHARED SETTINGS
+//////////////////////////////////////////////////////////////////////////////
+
+  lazy val commonSettings = Project.defaultSettings ++
+                            basicSettings ++
+                            formatSettings ++
+                            net.virtualvoid.sbt.graph.Plugin.graphSettings
+
+  lazy val basicSettings = Seq(
+    version := PROJECT_VERSION,
+    organization := ORGANIZATION,
+    scalaVersion := SCALA_VERSION,
+
+    libraryDependencies ++= Seq(
+      "org.apache.mesos" % "mesos"           % "0.15.0",
+      "com.typesafe"     % "config"          % "1.0.2",
+      "org.slf4j"        % "slf4j-api"       % "1.7.2",
+      "ch.qos.logback"   % "logback-classic" % "1.0.9" % "runtime",
+      "commons-cli" 	 % "commons-cli" 	 % "1.2",
+      "org.scalatest"   %% "scalatest"       % "2.0.M5b" % "test"
+    ),
+
+    scalacOptions in Compile ++= Seq(
+      "-unchecked",
+      "-deprecation",
+      "-feature"
+    ),
+
+    javaOptions += "-Djava.library.path=%s:%s".format(
+      sys.props("java.library.path"),
+      pathToMesosLibs
+    ),
+
+    fork in run := true,
+
+    fork in Test := true,
+
+    parallelExecution in Test := false
+  )
+
+  lazy val formatSettings = scalariformSettings ++ Seq(
+    ScalariformKeys.preferences := FormattingPreferences()
+      .setPreference(IndentWithTabs, false)
+      .setPreference(IndentSpaces, 2)
+      .setPreference(AlignParameters, false)
+      .setPreference(DoubleIndentClassDeclaration, true)
+      .setPreference(MultilineScaladocCommentsStartOnFirstLine, false)
+      .setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk, true)
+      .setPreference(PreserveDanglingCloseParenthesis, true)
+      .setPreference(CompactControlReadability, true)
+      .setPreference(AlignSingleLineCaseStatements, true)
+      .setPreference(PreserveSpaceBeforeArguments, true)
+      .setPreference(SpaceBeforeColon, false)
+      .setPreference(SpaceInsideBrackets, false)
+      .setPreference(SpaceInsideParentheses, false)
+      .setPreference(SpacesWithinPatternBinders, true)
+      .setPreference(FormatXml, true)
+  )
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,7 @@
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1")
+
+addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.1.2")
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
