f1c86654c1638efe3c6f15b67efb8e1ba7a562b8
Bernd Mathiske <bernd@mesosphere.io>
2014-07-22 13:40:36 -0700

    missing file WebServer

---
+++ src/main/scala/io/mesosphere/mesosaurus/WebServer.scala
@@ -0,0 +1,31 @@
+package io.mesosphere.mesosaurus
+
+import tiscaf._
+
+object WebServer extends HServer {
+
+    def _port = 8080
+
+    def ports = Set(_port)
+
+    def url(): String = {
+        return "http://" + java.net.InetAddress.getLocalHost().getHostAddress() + ":" + _port
+    }
+
+    override protected def startStopListener {
+        // do not start the daemon stop thread
+    }
+
+    object StaticApp extends HApp {
+        object StaticLet extends let.FsLet {
+            protected def dirRoot = "./task"
+        }
+
+        def resolve(req: HReqData) = Some(StaticLet)
+
+        override def buffered: Boolean = true // ResourceLet needs buffered or chunked be set
+    }
+
+    def apps = Seq(StaticApp)
+
+}
